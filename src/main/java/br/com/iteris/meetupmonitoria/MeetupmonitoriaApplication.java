94962e1f4918273e2d907d5d3ed03aa5be4d01eb
guina.costa <ice_aalmeida@uolinc.com>
2021-04-15 19:42:40 -0300

    first commit

---
+++ src/main/java/br/com/iteris/meetupmonitoria/MeetupmonitoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteris.meetupmonitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupmonitoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupmonitoriaApplication.class, args);
+	}
+
+}
